4e9030c555624cf3f1f07607cc62664d53cba013
jiyuhosi <jiyuhosi@gmail.com>
2024-11-24 10:07:57 +0900

    first commit

---
+++ db/index.ts
@@ -0,0 +1,6 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
+const sql: any = neon(process.env.DATABASE_URL!);
+const db = drizzle(sql);
+
+export default db;
